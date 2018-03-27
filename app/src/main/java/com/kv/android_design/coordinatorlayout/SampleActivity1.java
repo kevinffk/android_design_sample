c3a291657b5687f15b55291e6f0925e9e1c60e8e
fengfenkai <fengfenkai@icarbonx.com>
2018-03-27 18:54:15 +0800

    android design sample

---
+++ app/src/main/java/com/kv/android_design/coordinatorlayout/SampleActivity1.java
@@ -0,0 +1,24 @@
+package com.kv.android_design.coordinatorlayout;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+
+import com.kv.android_design.R;
+
+/**
+ * Author:  Kevin Feng
+ * Email:   fengfenkai@icarbonx.com
+ * Date:    2018/3/27
+ * Description:
+ */
+public class SampleActivity1 extends FragmentActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sample_1);
+    }
+
+
+}
